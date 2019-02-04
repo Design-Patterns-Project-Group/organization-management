8c5189136ec314a0efcd22178f97185944aba81c
unknown <mailtonahom2@gmail.com>
2019-02-05 02:51:04 +0300

    added product model

---
+++ data/Product.py
@@ -0,0 +1,17 @@
+class Product:
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+    def getId(self):
+        return self.id
+    def setId(self, id):
+        self.id = id
+    def getName(self):
+        return self.name
+    def setName(self, name):
+        self.name = name
+    def getPrice(self):
+        return self.price
+    def setPrice(self, price):
+        self.price = price

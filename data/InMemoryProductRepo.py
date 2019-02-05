6899dd6389620186f6fda4c0ee1591188cd14cb3
unknown <mailtonahom2@gmail.com>
2019-02-05 03:36:08 +0300

    Implemented ProductRepo in InMemoryProductRepo

---
+++ data/InMemoryProductRepo.py
@@ -0,0 +1,18 @@
+from ProductRepo import ProductRepo
+from Product import Product
+class InMemoryProductRepo(ProductRepo):
+    products = []
+    def getProductById(self, id):
+        for product in InMemoryProductRepo.products:
+            if product.id == id:
+                return product
+        return None
+    def insertProduct(self, product):
+        InMemoryProductRepo.products.append(product)
+    def deleteProduct(self, product):
+        for idx,pdt in enumerate(InMemoryProductRepo.products):
+            if pdt.id == product.id:
+                del InMemoryProductRepo.products[idx]
+                break
+    def getProducts(self):
+        return InMemoryProductRepo.products
